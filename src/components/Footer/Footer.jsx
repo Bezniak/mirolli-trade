cb0fd7e1a6bcf858e541e88182d72bbc92d271f5
ivanbezniak <ivan.bezniak@gmail.com>
2025-03-14 21:28:06 +0300

    6 commit Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -88,13 +88,13 @@ const Footer = () => {
                 </div>
 
                 <div className="px-4 py-6 bg-gray-900 md:flex md:items-center md:justify-between">
-                    <span className="text-base text-gray-500 dark:text-gray-300 sm:text-center font-light">
+                    <span className="text-base text-gray-500 text-center font-light">
                         © {year} <NavLink to={ROUTES.HOME} onClick={handleClick}>{t("companyName")}™</NavLink>.
                         &nbsp;
                         <a href="https://contragent.by/unp/591614779"
                            target="_blank"
                            rel="noopener noreferrer"
-                           className="hover:text-white transition font-light"
+                           className="hover:text-red-500 transition font-light"
                         >
                             {t("UNP")}
                         </a>
@@ -103,49 +103,17 @@ const Footer = () => {
                     </span>
 
                     <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-                        <a href="https://www.facebook.com/profile.php?id=100070173889307" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor" viewBox="0 0 8 19">
-                                <path fillRule="evenodd"
-                                      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                                      clipRule="evenodd"/>
-                            </svg>
-                            <span className="sr-only">Facebook page</span>
-                        </a>
-                        <a href="https://www.instagram.com/ivsoft.by" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <FaInstagramSquare className="w-6 h-6"/>
-                        </a>
-                        <a
-                            href="https://www.tiktok.com/@ivsoft.by"
-                            target='_blank'
-                            className="text-gray-400 hover:text-white"
-                        >
-                            <FaTiktok className="w-6 h-6"/>
-                        </a>
                         <a href="viber://chat?number=%2B375295210417"
                            target='_blank'
-                           className="text-gray-400 hover:text-white">
+                           className="text-gray-400 hover:text-red-500">
                             <FaViber className="w-6 h-6"/>
                         </a>
-                        <a href="https://github.com/Bezniak"
-                           target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd"
-                                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                                      clipRule="evenodd"/>
-                            </svg>
-                            <span className="sr-only">GitHub account</span>
-                        </a>
                         <a href="https://t.me/ivan_bezniak" target='_blank'
-                           className="text-gray-400 hover:text-white">
+                           className="text-gray-400 hover:text-red-500">
                             <FaTelegram className="w-6 h-6"/>
                         </a>
-                        <a href="mailto:ivsoft.ask@gmail.com" target='_blank'
-                           className="text-gray-400 hover:text-white">
+                        <a href="mailto:tvbr@tut.by" target='_blank'
+                           className="text-gray-400 hover:text-red-500">
                             <MdOutlineMailOutline className="w-6 h-6"/>
                         </a>
                     </div>
