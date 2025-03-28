ea82efc98d91e106550a795a2ac35ec76914f6e6
ivanbezniak <ivan.bezniak@gmail.com>
2025-03-28 15:37:39 +0300

    7 commit Service.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,14 @@ import './index.css'
 import './18n';
 import App from './App.jsx'
 import {AuthProvider} from "./context/AuthContext.jsx";
-import {HelmetProvider} from "react-helmet-async";
 import {Preloader} from "./components/Preloader/Preloader.jsx";
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
-        <HelmetProvider>
-            <Suspense fallback={<Preloader/>}>
-                <App/>
-            </Suspense>
-        </HelmetProvider>
+        {/*<HelmetProvider>*/}
+        <Suspense fallback={<Preloader/>}>
+            <App/>
+        </Suspense>
+        {/*</HelmetProvider>*/}
     </AuthProvider>
 )
